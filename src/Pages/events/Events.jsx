76823313e767d1680e8d4740eb09ec20c62b5895
shreyassanthu77 <shreyassanthu77@gmail.com>
2023-05-05 06:53:02 +0000

    feat: update events page

--- src/Pages/events/Events.jsx
+++ src/Pages/events/Events.jsx
@@ -36,15 +36,21 @@ function EventCard() {
 function Events() {
   return (
     <>
-      <main className="grid place-items-center min-h-[80vh] section">
+      <main className="flex flex-col justify-center items-center gap-4 min-h-[80vh] section">
         <h1 className="text-3xl font-bold max-w-[60ch] text-center">
           Events that help you grow
         </h1>
+        <a href="#events" className="px-4 py-2 text-white bg-black rounded-lg">
+          Check them out
+        </a>
       </main>
-      <section className="grid gap-6 p-6 bg-blue-900 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
-        <EventCard />
-        <EventCard />
-        <EventCard />
+      <section id="events" className="p-6 bg-blue-900">
+        <h2 className="text-2xl font-bold text-white">Events</h2>
+        <div className="grid gap-6 mt-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          <EventCard />
+          <EventCard />
+          <EventCard />
+        </div>
       </section>
     </>
   );
